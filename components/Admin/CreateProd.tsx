83eb56aca7888432344a9beca0b1bc206157eee4
moji94 <mojtababahadori94@gmail.com>
2023-04-02 19:00:35 +0430

    Update CreateProd

--- components/Admin/CreateProd.tsx
+++ components/Admin/CreateProd.tsx
@@ -4,7 +4,6 @@ import { useAtom } from 'jotai'
 
 export const CreateProd = (): JSX.Element => {
   const [ac, setAc] = useAtom(adminPos)
-  console.log(ac)
   return (
     <Container ac={ac}>
       {ac === 'Cprod' ? (
